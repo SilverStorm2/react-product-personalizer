83a6d77e55680a5f30521e0e58b6c4170ce3db17
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2025-11-02 17:12:42 +0100

    feat: add PropTypes and implement state for current color and size
    Added PropTypes for title and basePrice. Introduced component state for currentColor and currentSize with default initial values. Updated image src and alt to depend on selected product title and color.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,45 @@
+import { useEffect, useMemo, useState } from 'react';
 import styles from './Product.module.scss';
 import clsx from 'clsx';
+import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 
-const Product = props => {
+const Product = ({ name, title, basePrice, colors, sizes }) => {
+  const [currentColor, setCurrentColor] = useState(() => colors?.[0] ?? '');
+  const [currentSize, setCurrentSize] = useState(() => sizes?.[0]?.name ?? '');
+
+  useEffect(() => {
+    setCurrentColor(colors?.[0] ?? '');
+  }, [colors]);
+
+  useEffect(() => {
+    setCurrentSize(sizes?.[0]?.name ?? '');
+  }, [sizes]);
+
+  const productImageSrc = useMemo(() => {
+    if (!name || !currentColor) {
+      return `${process.env.PUBLIC_URL}/images/products/shirt-missing.jpg`;
+    }
+
+    return `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`;
+  }, [name, currentColor]);
+
   return (
-    <article className={styles.product}>
+    <article
+      className={styles.product}
+      data-current-color={currentColor}
+      data-current-size={currentSize}
+    >
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={productImageSrc} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -41,6 +66,20 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      additionalPrice: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Product;
